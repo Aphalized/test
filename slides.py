4c578b548b5141f25ed9d05426a24caa9b8c5b39
Aphalized <alexathens1910@gmail.com>
2024-12-01 16:26:35 +0200

    Update slides.py

--- slides.py
+++ slides.py
@@ -10,29 +10,24 @@ greek.add_to_preamble(r"""
 
 class Introduction(Slide):
     def construct(self):
-        welcome = Text("This is the Manim Slides starter")
-        square = Square(color=BLUE)
-        dot = Dot(color=RED).shift(RIGHT + UP)
-
-        self.play(FadeIn(welcome))
-        self.next_slide()
-
-        self.wipe(welcome, square)
-        self.play(FadeIn(dot))
-
-        self.next_slide(loop=True)
-        self.play(
-            MoveAlongPath(dot, square, rate_func=linear), run_time=2
-        )
-
-class WithTeX(Slide):
-    def construct(self):
         Title = Text("Τριγωνομετρική απόδειξη του", line_spacing=1, font="Times New Roman", font_size=40).shift(UP*1)
         self.play(FadeIn(Title))
         Title2 = Text("Πυθαγορείου Θεωρήματος", line_spacing=1, font="Times New Roman", font_size=60)
         Title2.set_color_by_gradient(BLUE, LIGHT_PINK, RED)
         self.next_slide()
         self.play(Write(Title2), run_time=3)
+
+class WithTeX(Slide):
+        def construct(self):
+        im = MathTex(r"i^i = e^{-\frac{\pi}{2}}").scale(2)
+        im.set_color_by_gradient(BLUE, LIGHT_PINK, RED)
+        giati = Text("Γιατί όμως;", line_spacing=1, font="Times New Roman", font_size=35).shift(UP * 1)
+        self.play(Write(im), run_time=3)
+        self.next_slide()
+        self.play(Write(giati))
+        self.next_slide()
+        self.play(im.animate.shift(UP * 6.5), FadeOut(giati))
+        self.next_slide()
 
 
 class Outro(Slide):
